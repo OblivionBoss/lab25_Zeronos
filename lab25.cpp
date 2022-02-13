59a2c4f6d2d85de32b4a4aaf631f9ec455cf31f6
Zeronos <boss16805@gmail.com>
2022-02-13 15:22:01 +0700

    lab25 completed

--- lab25.cpp
+++ lab25.cpp
@@ -29,6 +29,85 @@ ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 }
 
 //Write your code here
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+    ComplexNumber z;
+    z.real = real*c.real - imag*c.imag;
+    z.imag = c.real*imag + real*c.imag;
+	return z;
+}
+
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+    ComplexNumber z;
+    double s = pow(c.real,2) + pow(c.imag,2);
+    z.real = (real*c.real + imag*c.imag)/s;
+    z.imag = (c.real*imag - real*c.imag)/s;
+	return z;
+}
+
+bool ComplexNumber::operator==(const ComplexNumber &c){
+    if(c.real == real && c.imag == imag){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+double ComplexNumber::abs(){
+    double s = pow(real,2) + pow(imag,2);
+    return sqrt(s);
+}
+
+double ComplexNumber::angle(){
+    double k = atan2(imag,real) * 180 / M_PI;
+    return k;
+}
+
+ComplexNumber operator+(double p,const ComplexNumber &c){
+	return ComplexNumber(p+c.real,c.imag);
+}
+
+ComplexNumber operator-(double p,const ComplexNumber &c){
+	return ComplexNumber(p-c.real,-c.imag);
+}
+
+ComplexNumber operator*(double p,const ComplexNumber &c){
+    ComplexNumber z;
+    z.real = p*c.real;
+    z.imag = p*c.imag;
+	return z;
+}
+
+ComplexNumber operator/(double p,const ComplexNumber &c){
+    ComplexNumber z;
+    double s = pow(c.real,2) + pow(c.imag,2);
+    z.real = (p*c.real)/s;
+    z.imag = (p*-c.imag)/s;
+	return z;
+}
+
+bool operator==(double p,const ComplexNumber &c){
+    if(c.real == p && c.imag == p){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+ostream & operator<<(ostream &gtfo, const ComplexNumber &c){ //imag is 1 become only i 
+    if(c.imag > 0){
+        if(c.real == 0) return gtfo << c.imag << "i";
+        else return gtfo << c.real << "+" << c.imag << "i";
+    }
+    else if(c.imag == 0){
+        return gtfo << c.real;
+    }
+    else{
+        if(c.real == 0) return gtfo << c.imag << "i";
+        else return gtfo << c.real << c.imag << "i";
+    }
+}
 
 int main(){
 	ComplexNumber a(1.5,2),b(3.2,-2.5),c(-1,1.2);	
